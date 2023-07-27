cfc6bb36276599058f86a9ee232f1849e7ff6370
Konrad Mocken <konradmocken@gmail.com>
2023-07-27 19:08:10 -0300

    segunda entrega

--- app.js
+++ app.js
@@ -1,29 +1,64 @@
-let clientes = " Bienvenidos nuevos Clientes: \n"
-let cantidad_clientes = 0
-let rta = ""
-let elejir = prompt("bienvenido, elija una opcion\na: Wifi\nb: Fibra")
-if ((elejir == "a") || (elejir == "b")){
-
-}else{
-  alert ("Opcion no valida")
-  
-}
+let clientes = [];
+let cantidad_clientes = 0;
+let mostrar = "";
 
 function saludar(nombre) {
-    alert("¡Hola, " + nombre + "!");
-  }
-do{
-    
-    let nombre = prompt("Ingrese su nombre")
-    let apellido = prompt("Ingrese su apellido")    
-    let edad = Number(prompt("Ingrese su edad"))
-    let direccion = prompt("Ingrese su Direccion")
-    clientes = clientes + "\nNombre: "+ nombre + "\nApellido: "+ apellido + "\nEdad: "+ edad +"\nDireccion: " + direccion
-    rta = prompt("¿Desea salir?(Escriba 'ESC' para salir)").toUpperCase()
-    cantidad_clientes= cantidad_clientes + 1
-}while(rta != "ESC")
+  alert("¡Hola, " + nombre + "!");
+}
 
-saludar ("clientes")
+const opciones = [
+  { codigo: "a", tipo: "Wifi" },
+  { codigo: "b", tipo: "Fibra" },
+];
 
-alert(clientes)
-let total = alert("total de clientes\n" + cantidad_clientes)
+function obtenerTipo(codigo) {
+  return opciones.find((opcion) => opcion.codigo === codigo).tipo;
+}
+
+function ingresarCliente() {
+  let cliente = {};
+  cliente.nombre = prompt("Ingrese su nombre");
+  cliente.apellido = prompt("Ingrese su apellido");
+  cliente.edad = Number(prompt("Ingrese su edad"));
+  cliente.direccion = prompt("Ingrese su dirección");
+  cliente.tipo = obtenerTipo(elejir);
+  return cliente;
+}
+
+function mostrarClientes(clientes) {
+  let mensaje = "Bienvenidos nuevos Clientes:\n";
+  clientes.forEach((cliente) => {
+    mensaje +=
+      "\nNombre: " +
+      cliente.nombre +
+      "\nApellido: " +
+      cliente.apellido +
+      "\nEdad: " +
+      cliente.edad +
+      "\nDireccion: " +
+      cliente.direccion +
+      "\nTipo: " +
+      cliente.tipo;
+  });
+  return mensaje;
+}
+
+let elejir = prompt(
+  "Bienvenido Nuevos Clientes, elija una opción:\na: Wifi\nb: Fibra"
+).toLowerCase();
+
+const opcionesValidas = opciones.map((opcion) => opcion.codigo);
+if (opcionesValidas.includes(elejir)) {
+  do {
+    clientes.push(ingresarCliente());
+    cantidad_clientes += 1;
+    mostrar = clientes[clientes.length - 1].nombre;
+    rta = prompt("¿Desea salir? (Escriba 'ESC' para salir)").toUpperCase();
+  } while (rta !== "ESC");
+
+  saludar(mostrar);
+  alert(mostrarClientes(clientes));
+  alert("Total de clientes: " + cantidad_clientes);
+} else {
+  alert("Opción no válida");
+}
